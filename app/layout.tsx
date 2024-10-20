e75c9a72c574eff55003c10d50cb0eba4322f1d2
unknown <hossenliakat19@gmail.com>
2024-10-20 21:31:42 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import { inter } from '@/app/ui/font';
+import '@/app/ui/global.css';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
